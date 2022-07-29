8b9d2ee3a97e890c896b143095881506cae640ba
OnlyWay <87950246@qq.com>
2022-07-29 19:06:26 +0800

    生命周期

--- vue.js
+++ vue.js
@@ -1,21 +1,36 @@
 class Vue {
   constructor(options) {
-    this.$el = document.querySelector(options.el);
+
+    if( typeof options.beforeCreate == 'function'){
+      options.beforeCreate.bind(this)()
+    } 
+    // 这是dota
     this.$data = options.data;
-    console.log(this.$el);
+    if( typeof options.created == 'function'){
+      options.created.bind(this)()
+    }
+    if( typeof options.beforeMount == 'function'){
+      options.beforeMount.bind(this)()
+    }
+    // 这是节点
+    this.$el = document.querySelector(options.el);
+    // 解析模板
     this.compile(this.$el);
+    if( typeof options.mounted == 'function'){
+      options.mounted.bind(this)()
+    }
   }
 
   compile(node) {
-    node.childNodes.forEach((item, index) => {
-      //如果是文本节点，就把{{ }}里面的内容替换成数据
+    node.childNodes.forEach((item) => {
+      //如果是其他节点，就继续递归解析
       if (item.nodeType == 1) {
         this.compile(item);
       }
+      //如果是文本节点，就把{{ }}里面的内容替换成数据
       if (item.nodeType == 3) {
         // 正则匹配 {{}}
         let reg = /\{\{(.*?)\}\}/g;
-        console.log(item);
         let text = item.textContent;
         // 赋值
         item.textContent = text.replace(reg, (match, vmKey) => {
